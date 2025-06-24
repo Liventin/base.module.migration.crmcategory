9a2593d61f3f119511a45c6d3a9dd614fc76d999
a.odegov <a.odegov@oos.ru>
2025-06-24 10:20:25 +0300

    release v1.0.1

--- lib/Service/Migration/CrmCategory/MigrateCrmCategoryService.php
+++ lib/Service/Migration/CrmCategory/MigrateCrmCategoryService.php
@@ -4,7 +4,7 @@ namespace Base\Module\Service\Migration\CrmCategory;
 
 interface MigrateCrmCategoryService
 {
-    public const SERVICE_CODE = 'base.module.migration.crm.category';
+    public const SERVICE_CODE = 'base.module.migration.crm.category.service';
 
     public function setCategoriesList(array $categories): self;
     public function install(): void;
--- service_locator/migration.crmcategory.php
+++ service_locator/migration.crmcategory.php
@@ -1,11 +1,9 @@
 <?php
 defined('B_PROLOG_INCLUDED') || die;
 
-use Base\Module\Service\Migration\CrmCategory\MigrateCrmCategoryService;
-
 return [
-    'base.module.migration.smart.process' => [
-        'className' => MigrateCrmCategoryService::class,
+    'base.module.migration.crm.category.service' => [
+        'className' => \Base\Module\Service\Migration\CrmCategory\MigrateCrmCategoryService::class,
         'constructorParams' => [],
     ],
 ];
