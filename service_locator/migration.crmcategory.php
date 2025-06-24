043a6b3c6dd86f7e533a52d3b8c392155196de48
a.odegov <a.odegov@oos.ru>
2025-06-24 10:33:25 +0300

    release v1.0.2

--- service_locator/migration.crmcategory.php
+++ service_locator/migration.crmcategory.php
@@ -3,7 +3,7 @@ defined('B_PROLOG_INCLUDED') || die;
 
 return [
     'base.module.migration.crm.category.service' => [
-        'className' => \Base\Module\Service\Migration\CrmCategory\MigrateCrmCategoryService::class,
+        'className' => \Base\Module\Src\Migration\CrmCategory\MigrateCrmCategoryService::class,
         'constructorParams' => [],
     ],
 ];
